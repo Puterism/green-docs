471f477cae12b42f69406553d63e4ca30b468db2
Shim MunSeong <Puterism.K@gmail.com>
2022-08-21 05:32:23 +0900

    feat: Assignee를 불러올 수 있는 기능 구현

--- tabs/src/pages/Home/Home.js
+++ tabs/src/pages/Home/Home.js
@@ -1,11 +1,11 @@
-import { DatePicker, Text, TextField, PrimaryButton, NormalPeoplePicker } from '@fluentui/react';
+import { DatePicker, Text, TextField, PrimaryButton } from '@fluentui/react';
 import Layout from '../../components/Layout/Layout';
 import Sidebar from '../../components/Sidebar/Sidebar';
 import Styled from './Home.styles';
 
 import HeadTitle from '../../components/HeadTitle/HeadTitle';
 import { Pivot, PivotItem } from '@fluentui/react';
-
+import Overview from '../Overview/Overview';
 import usePeoplePicker from '../../hooks/usePeoplePicker';
 import useInput from '../../hooks/useInput';
 import useDatePicker from '../../hooks/useDatePicker';
@@ -22,7 +22,6 @@ const Home = () => {
   const { picker, onFilterChanged, returnMostRecentlyUsed, onRemoveSuggestion, onItemChange } =
     usePeoplePicker();
 
-  const [loading, setIsLoading] = useState(false);
   const [objectiveName, onChangeObjectiveName] = useInput('');
   const [dueDate, onSelectDueDate] = useDatePicker('');
   const [description, onChangeDescription] = useInput('');
@@ -32,6 +31,75 @@ const Home = () => {
   };
 
   const isValidForm = objectiveName.trim() !== '';
+
+  const handleInputChange = (event) => {
+    console.log(event.target.selectedPeople);
+  };
+
+  const [showLoginPage, setShowLoginPage] = useState(false);
+
+  const teamsfx = useRef(null);
+  const scope = useRef(null);
+
+  const initGraphToolkit = useCallback(async (teamsfx, scope) => {
+    console.log('initGraphToolkit');
+    const provider = new TeamsFxProvider(teamsfx, scope);
+    Providers.globalProvider = provider;
+  }, []);
+
+  const initTeamsFx = useCallback(async () => {
+    console.log('initTeamsFx');
+    teamsfx.current = new TeamsFx();
+
+    // Only these two permission can be used without admin approval in microsoft tenant
+    scope.current = ['User.Read', 'User.ReadBasic.All'];
+  }, []);
+
+  const checkIsConsentNeeded = useCallback(async () => {
+    console.log('checkIsConsentNeeded');
+    let consentNeeded = false;
+    try {
+      await teamsfx.current.getCredential().getToken(scope.current);
+    } catch (error) {
+      consentNeeded = true;
+    }
+
+    setShowLoginPage(consentNeeded);
+
+    Providers.globalProvider.setState(
+      consentNeeded ? ProviderState.SignedOut : ProviderState.SignedIn
+    );
+    return consentNeeded;
+  }, []);
+
+  const loginBtnClick = useCallback(async () => {
+    try {
+      await teamsfx.current.login(scope.current);
+      Providers.globalProvider.setState(ProviderState.SignedIn);
+      setShowLoginPage(false);
+    } catch (err) {
+      if (err.message?.includes('CancelledByUser')) {
+        const helpLink = 'https://aka.ms/teamsfx-auth-code-flow';
+        err.message +=
+          '\nIf you see "AADSTS50011: The reply URL specified in the request does not match the reply URLs configured for the application" ' +
+          'in the popup window, you may be using unmatched version for TeamsFx SDK (version >= 0.5.0) and Teams Toolkit (version < 3.3.0) or ' +
+          `cli (version < 0.11.0). Please refer to the help link for how to fix the issue: ${helpLink}`;
+      }
+
+      alert('Login failed: ' + err);
+      return;
+    }
+  }, []);
+
+  const initialize = useCallback(async () => {
+    await initTeamsFx();
+    await initGraphToolkit(teamsfx.current, scope.current);
+    await checkIsConsentNeeded();
+  }, [checkIsConsentNeeded, initGraphToolkit, initTeamsFx]);
+
+  useEffect(() => {
+    initialize();
+  }, [initialize]);
 
   return (
     <Layout>
@@ -95,35 +163,29 @@ const Home = () => {
           </Styled.Form>
         </Styled.SidebarContent>
       </Sidebar>
-      {loading ? (
-        <EmptyScreen
-          title={'Objectives created in this workspace will appear here'}
-          image={'CheckmarkWaiter'}
-        ></EmptyScreen>
-      ) : (
-        <Styled.Home>
-          <HeadTitle name={'sonminji'} />
-          <Styled.PivotWrapper>
-            <Pivot aria-label="Basic Pivot Example">
-              <PivotItem headerText="Overview">
-                <Overview />
-              </PivotItem>
 
-              <PivotItem headerText="Objectives">
-                <Overview />
-              </PivotItem>
+      <Styled.Home>
+        <HeadTitle name={'sonminji'} />
+        <Styled.PivotWrapper>
+          <Pivot aria-label="Basic Pivot Example">
+            <PivotItem headerText="Overview">
+              <Overview />
+            </PivotItem>
 
-              <PivotItem headerText="Tasks">
-                <Overview />
-              </PivotItem>
+                <PivotItem headerText="Objectives">
+                  <Overview />
+                </PivotItem>
 
-              <PivotItem headerText="Sub Tasks">
-                <Overview />
-              </PivotItem>
-            </Pivot>
-            <Link to="/detail/1">Detail</Link>
-          </Styled.PivotWrapper>
-        </Styled.Home>
+                <PivotItem headerText="Tasks">
+                  <Overview />
+                </PivotItem>
+
+            <PivotItem headerText="Sub Tasks">
+              <Overview />
+            </PivotItem>
+          </Pivot>
+        </Styled.PivotWrapper>
+      </Styled.Home>
     </Layout>
   );
 };

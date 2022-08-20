d4b306a86e29a169ba104234f9e3d491def64577
Shim MunSeong <Puterism.K@gmail.com>
2022-08-21 01:44:53 +0900

    fix: 아이콘이 나타나지 않는 문제 수정

--- tabs/src/App.js
+++ tabs/src/App.js
@@ -2,13 +2,15 @@ import { HashRouter, Switch, Route, Redirect } from 'react-router-dom';
 import Detail from './pages/Detail/Detail';
 import Home from './pages/Home/Home';
 
-import { ThemeProvider } from '@fluentui/react';
+import { initializeIcons, ThemeProvider } from '@fluentui/react';
 
 const appTheme = {
   palette: {
     themePrimary: '#019e79',
   },
 };
+
+initializeIcons();
 
 const App = () => {
   return (

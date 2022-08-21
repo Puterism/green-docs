1339ff93486dd585b5d9b00a4855459a77912c4e
limetree06 <limetree06@unist.ac.kr>
2022-08-21 10:57:35 +0900

    add : download csv

--- tabs/src/components/HeadTitle/HeadTitle.js
+++ tabs/src/components/HeadTitle/HeadTitle.js
@@ -2,17 +2,61 @@ import Styled from './HeadTitle.styles';
 import { useEffect } from 'react';
 import { CSVLink } from 'react-csv';
 import { PrimaryButton } from '@fluentui/react';
+import axios from 'axios';
 
 const csvData = [
-  ['Ahmed', 'Tomi', 'ah@smthing.co.com'],
-  ['Raed', 'Labes', 'rl@smthing.co.com'],
-  ['Yezzi', 'Min l3b', 'ymin@cocococo.com'],
+  ['OKR', 'title', 'score', 'Progress'],
+  ['Objective 1', 'junction asia', '1', '100'],
+
+  ['Key Value 1', 'mission 1 - choose track', 2, 100],
+
+  ['Key Value 1-1', 'ideation', 1, 100],
+  ['Key Value 1-2', 'discussion', 3, 100],
+  ['Key Value 1-3', 'choose', 5, 100],
+  [],
+  ['Key Value 2', 'mission 2 - First Draft Submission', 3, 100],
+  ['Key Value 2-1', 'ppt & descrription file', 5, 100],
+  ['Key Value 2-2', 'set up', 3, 100],
+  ['Key Value 2-3', 'desgin draft', 3, 100],
+  [],
+  ['Key Value 3', 'mission 3 - First Draft Submission', 5, 80],
+  ['Key Value 3-1', 'Front UI', 3, 80],
+  ['Key Value 3-2', 'Back API', 3, 80],
+  ['Key Value 3-3', 'deploy', 1, 0],
+  ['Key Value 3-4', 'design', 3, 100],
+  ['Key Value 3-4', 'prepare for presentation', 3, 100],
+  [],
 ];
 
-const headers = ['First Name', 'Last Name', 'Email', 'Address', 'Postcode'];
+const getSubTask = async (id) => {
+  try {
+    const response = await axios.get(`http://localhost:8888/tasks/${id}`);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const getObjectives = async (id) => {
+  let idList = [];
+  try {
+    const response = await axios.get(`http://localhost:8888/tasks?parentTaskId=${id}&order=DESC`);
+    const tasksList = response.data.data;
+
+    Object.values(tasksList).map((item) => idList.push(item.id));
+
+    if (idList.length === 0) {
+      getSubTask(id).then((response) => console.log('getSubTask', response));
+    } else {
+      idList.map((id) => getObjectives(id));
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
 const HeadTitle = ({ name }) => {
   useEffect(() => {
-    //전체데이터 호출 -> 엑셀로 익스포트
+    //getObjectives(1);
   }, []);
 
   return (
@@ -23,7 +67,7 @@ const HeadTitle = ({ name }) => {
           Welcome to Junction Asia workspace. Find your goals and tasks!
         </Styled.Subtitle>
       </div>
-      <CSVLink headers={headers} data={csvData} filename={'download'}>
+      <CSVLink data={csvData} filename={'download OKR'}>
         <PrimaryButton variant="success">Download CSV</PrimaryButton>
       </CSVLink>
     </Styled.Content>
